51636fa0b93e2c98f3af86821a13026498de6ed7
Thiago Elias <thiago@upitservices.com.br>
2019-07-20 17:18:50 -0300

    Filtering option

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,20 +1,36 @@
 <template>
-
-<div v-masonry transition-duration="0.3s" item-selector=".item">
-    <div v-masonry-tile class="item">
-      <CepEndereco/>
+  <div>
+    <div v-masonry transition-duration="0.3s" item-selector=".item">
+      <div v-masonry-tile class="item" v-for="tile in tiles" :key="tile.selector">
+        <CepEndereco v-if="tile.selector == 'cep-endereco' && tile.active == true" />
+        <EnderecoCep v-if="tile.selector == 'endereco-cep' && tile.active == true" />
+        <Bcrypt v-if="tile.selector == 'bcrypt-generator' && tile.active == true" />
+        <BcryptDecrypt v-if="tile.selector == 'bcrypt-decrypt' && tile.active == true" />
+      </div>
     </div>
-    <div v-masonry-tile class="item">
-      <EnderecoCep/>
-    </div>
-    <div v-masonry-tile class="item">
-      <Bcrypt/>
-    </div>
-    <div v-masonry-tile class="item">
-      <BcryptDecrypt/>
-    </div>
-</div>
-
+    <v-bottom-sheet v-model="sheet">
+      <template v-slot:activator>
+        <v-btn color="blue darken-3" dark small fixed top right fab>
+          <v-icon>fas fa-filter</v-icon>
+        </v-btn>
+      </template>
+      <v-list>
+        <v-subheader>
+          <v-btn color="blue darken-3" dark small right @click="sheet = false">Fechar</v-btn>
+        </v-subheader>
+        <v-list-tile v-for="(tile, index) in tiles" :key="tile.title">
+          <v-list-tile-avatar>
+            <v-avatar size="32px" tile>
+              <v-icon>{{ tile.icon }}</v-icon>
+            </v-avatar>
+          </v-list-tile-avatar>
+          <div @click="lazyRedraw()">
+            <v-switch v-model="tiles[index].active" :label="tile.title"></v-switch>
+          </div>
+        </v-list-tile>
+      </v-list>
+    </v-bottom-sheet>
+  </div>
 </template>
 
 <script>
@@ -22,10 +38,39 @@ import CepEndereco from "./CepEndereco";
 import EnderecoCep from "./EnderecoCep";
 import Bcrypt from "./Bcrypt";
 import BcryptDecrypt from "./BcryptDecrypt";
-import { EventBus } from "@/main.js"
+import { EventBus } from "@/main.js";
+import { setTimeout } from "timers";
 
 export default {
-  data: () => ({}),
+  data: () => ({
+    sheet: false,
+    tiles: [
+      {
+        selector: "cep-endereco",
+        icon: "fas fa-map-marker",
+        title: "CEP pelo Endereço",
+        active: true
+      },
+      {
+        selector: "endereco-cep",
+        icon: "fas fa-city",
+        title: "Endereço pelo CEP",
+        active: true
+      },
+      {
+        selector: "bcrypt-generator",
+        icon: "fas fa-key",
+        title: "Bcrypt Generator",
+        active: true
+      },
+      {
+        selector: "bcrypt-decrypt",
+        icon: "fas fa-key",
+        title: "Bcrypt Decrypt",
+        active: true
+      }
+    ]
+  }),
   components: {
     CepEndereco,
     EnderecoCep,
@@ -33,14 +78,18 @@ export default {
     BcryptDecrypt
   },
   created: function() {
-    EventBus.$on('redraw', this.redraw);
+    EventBus.$on("redraw", this.redraw);
   },
   beforeDestroy: function() {
-    EventBus.off('redraw');
+    EventBus.off("redraw");
   },
   methods: {
+    lazyRedraw() {
+      setTimeout(() => {
+        this.redraw();
+      }, 250);
+    },
     redraw() {
-      console.log('Redrawing Grid');
       this.$redrawVueMasonry();
     }
   }
